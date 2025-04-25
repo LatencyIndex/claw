112bec6b731fc9811ed15bfe655c2decf227c1ea
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:08 +0200

    Export Data.List.sortOn in Claw.Control

--- src/Claw/Control.hs
+++ src/Claw/Control.hs
@@ -6,12 +6,13 @@ module Claw.Control (
     isSuffixOf,
     mapBoth,
     sort,
+    sortOn,
 ) where
 
 import Control.Monad ((<=<))
 import Data.Function ((&))
 import Data.Functor ((<&>))
-import Data.List (isPrefixOf, isSuffixOf, sort)
+import Data.List (isPrefixOf, isSuffixOf, sort, sortOn)
 
 mapBoth :: (a -> b) -> (a, a) -> (b, b)
 mapBoth f (x, y) = (f x, f y)
