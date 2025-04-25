b3f777608d4699fe65e70667a6cfc1b52391b911
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:07 +0200

    Add Claw.Control.mapBoth

--- src/Claw/Control.hs
+++ src/Claw/Control.hs
@@ -5,9 +5,13 @@ module Claw.Control (
     isPrefixOf,
     isSuffixOf,
     sort,
+    mapBoth,
 ) where
 
 import Control.Monad ((<=<))
 import Data.Function ((&))
 import Data.Functor ((<&>))
 import Data.List (isPrefixOf, isSuffixOf, sort)
+
+mapBoth :: (a -> b) -> (a,a) -> (b,b)
+mapBoth f (x,y) = (f x, f y)
