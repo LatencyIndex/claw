00669a9381d6bfefca32ab300f52d2160c070abd
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:07 +0200

    Document showTime function

--- src/Claw/Time.hs
+++ src/Claw/Time.hs
@@ -11,5 +11,6 @@ import Data.Time.Clock (UTCTime)
 import Data.Time.Format (FormatTime, defaultTimeLocale, formatTime)
 import Data.Time.LocalTime (TimeZone, ZonedTime, getCurrentTimeZone, utcToZonedTime)
 
+-- | Show time in yyyy-mm-dd hh:mm format.
 showTime :: (FormatTime t) => t -> String
 showTime = formatTime defaultTimeLocale "%Y-%m-%d %H:%M"
