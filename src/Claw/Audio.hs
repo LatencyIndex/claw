5f9080f4ed53004912b91e40548e54f591a52c8e
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:08 +0200

    Audio: move decision between convert or copy to ffconvert

--- src/Claw/Audio.hs
+++ src/Claw/Audio.hs
@@ -8,8 +8,12 @@ import System.Process (callProcess)
 import System.Directory (copyFile)
 
 -- | Convert to destination file, with type determined by extension.
+-- If source and destination types are the same, skip conversion and just copy.
 ffconvert :: FilePath -> FilePath -> IO ()
-ffconvert src_file dst_file = callProcess "ffmpeg" ["-i", src_file, dst_file]
+ffconvert src_file dst_file =
+    if getExt src_file == getExt dst_file
+    then copyFile src_file dst_file
+    else callProcess "ffmpeg" ["-i", src_file, dst_file]
 
 -- | Convert file to mp3 to destination directory.
 -- If src_file is @src_dir/file.flac@, destination file will be @dst_dir/file.mp3@
@@ -17,8 +21,4 @@ ffconvert src_file dst_file = callProcess "ffmpeg" ["-i", src_file, dst_file]
 toMp3 :: FilePath -> FilePath -> IO FilePath
 toMp3 dst_dir src_file =
     let dst_file = dst_dir </> getBaseName src_file <.> "mp3"
-    in do
-        if getExt src_file == ".mp3"
-        then copyFile src_file dst_file
-        else ffconvert src_file dst_file
-        return dst_file
+    in ffconvert src_file dst_file >> return dst_file
