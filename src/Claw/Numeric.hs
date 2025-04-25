a1204c137ba357883ee98ffa2cc370bc7d7b29fe
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:07 +0200

    Add Claw.Numeric module

---
+++ src/Claw/Numeric.hs
@@ -0,0 +1,7 @@
+module Claw.Numeric (
+    mulRII
+) where
+
+-- | Multiply an integral with a real, and round back to integral.
+mulRII :: (RealFrac x, Integral n) => x -> n -> n
+mulRII x = round . (*x) . fromIntegral
