ae221c837189ec2ecbca56ef4f0e9f35a7d098c4
LatencyIndex <latency.index@gmail.com>
2025-04-25 21:10:07 +0200

    Add showDuration

--- src/Claw/Time.hs
+++ src/Claw/Time.hs
@@ -1,16 +1,28 @@
 module Claw.Time (
+    Duration,
     TimeZone,
     UTCTime,
     ZonedTime,
     getCurrentTimeZone,
+    showDuration,
     showTime,
     utcToZonedTime,
 ) where
 
-import Data.Time.Clock (UTCTime)
+import Data.Time.Clock (DiffTime, UTCTime)
 import Data.Time.Format (FormatTime, defaultTimeLocale, formatTime)
 import Data.Time.LocalTime (TimeZone, ZonedTime, getCurrentTimeZone, utcToZonedTime)
 
+type Duration = DiffTime
+
+-- | Format according to format string, per https://hackage.haskell.org/package/time/docs/Data-Time-Format.html#v:formatTime
+fmtTime :: (FormatTime t) => String -> t -> String
+fmtTime = formatTime defaultTimeLocale
+
 -- | Show time in yyyy-mm-dd hh:mm format.
 showTime :: (FormatTime t) => t -> String
-showTime = formatTime defaultTimeLocale "%Y-%m-%d %H:%M"
+showTime = fmtTime "%Y-%m-%d %H:%M"
+
+-- | Show duration in h:mm:ss format.
+showDuration :: Duration -> String
+showDuration = fmtTime "%h:%M:%S"
